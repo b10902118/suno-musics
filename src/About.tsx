64f28b57b89e838e6cce097566ace7c6ef44534c
b10902118 <b10902118@ntu.edu.tw>
2025-08-04 10:15:02 +0800

    Improved about.

--- src/About.tsx
+++ src/About.tsx
@@ -7,14 +7,19 @@ export default function About() {
     setMenu();
   }, []);
 
+  const pClassName = "text-[4vh]";
   return (
-    <div className="h-full w-full flex items-start justify-start bg-white">
-      <div className="h-full w-full px-[6vw] py-[6vh] bg-gray-50 overflow-y-auto">
-        <h1 className="text-3xl font-bold mb-4">About</h1>
-        <p>Images are from pxhere.com</p>
-        <br />
-        <p>Icon is from flaticon.com by SeyfDesigner</p>
-      </div>
+    <div className="h-full w-full px-[6vw] py-[6vh] bg-gray-50 overflow-y-auto">
+      <p className="text-[8vh] font-bold mb-4">About</p>
+      <p className={pClassName}>Images are updated everyday!</p>
+      <p className="my-2" />
+      <p className="text-[4vh] font-bold my-2">References:</p>
+      <ul className="list-disc pl-[6vw]">
+        <li className={pClassName}>Images are from pxhere.com</li>
+        <li className={pClassName}>
+          Icon is from flaticon.com by SeyfDesigner
+        </li>
+      </ul>
     </div>
   );
 }
